19b53097496083ba493b26cf2dde97544a9f4fb7
Matt <joshenpolen@gmail.com>
2025-03-10 03:09:28 +0800

    updated PM again

--- service-client/dist/assets/permitted-BzPEUTiR.js
+++
@@ -1,1 +0,0 @@
-import{a as s,ak as o,aK as e}from"./index-C8ph-EQq.js";const c=()=>{const t=s(),i=o(()=>t.abilities),{can:a}=e(i.value);return{can:a}};export{c as p};
--- service-client/vite.config.js
+++ service-client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'mattbacud07',
+  base: 'stest',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
